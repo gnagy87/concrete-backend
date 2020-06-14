34adf9dea74236dff6c1af24e6095a95ef674973
Gergely Nagy <gergely.nagy1205@gmail.com>
2020-06-14 23:56:14 +0200

    config(app) package system + 1 dummy endpoint

--- src/main/java/com/concrete/poletime/controllers/UserControllerAPI.java
+++ src/main/java/com/concrete/poletime/controllers/UserControllerAPI.java
@@ -1,4 +1,16 @@
 package com.concrete.poletime.controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
 public class UserControllerAPI {
+
+    @GetMapping("/")
+    public ResponseEntity helloApp() {
+        return ResponseEntity.ok().body("Hello World!");
+    }
 }
