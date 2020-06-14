626c9494e3830113173802f774b8b291891a6b10
Gergely Nagy <gergely.nagy1205@gmail.com>
2020-06-14 23:09:08 +0200

    config(app) empty project + db setup

---
+++ src/main/java/com/concrete/poletime/PoletimeApplication.java
@@ -0,0 +1,13 @@
+package com.concrete.poletime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoletimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoletimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/concrete/poletime/PoletimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.concrete.poletime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoletimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
