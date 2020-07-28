0f9a1afa4cd38cf324b41974fce7c4c56c5bfd19
Bando85 <Bando85>
2020-07-28 18:29:45 +0200

    - use saveUser method in PoleUserServiceImpl instead of poleUserRepo.save to handle persistence exceptions globally - email and password validation is unnecessary after valid registration - use exceptions with default messages, reduce duplications - unnecessary to store participants in Training, count PoleUsers instead

--- src/main/java/com/concrete/poletime/exceptions/RecordNotFoundException.java
+++ src/main/java/com/concrete/poletime/exceptions/RecordNotFoundException.java
@@ -4,4 +4,8 @@ public class RecordNotFoundException extends Exception {
     public RecordNotFoundException(String message) {
         super(message);
     }
+    public RecordNotFoundException() {
+        super("Record Not Found!");
+    }
+
 }
